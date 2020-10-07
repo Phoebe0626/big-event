1e738423942c2dddad3f4bc91774903c2dc659c7
phoebe <506014196@qq.com>
2020-10-07 18:53:18 +0800

    新增了修改分类功能

--- assets/js/category.js
+++ assets/js/category.js
@@ -37,3 +37,39 @@ $('.addbtn').on('click', function () {
     })
   })
 });
+
+// ----------------------------------编辑类别-----------------------------------------
+$('tbody').on('click', 'button:contains("编辑")', function () {
+  let html = $('#tpl_edit').html();
+  let data = $(this).data();
+  data.Id = data.id;
+  // 弹出添加框
+  let index = layer.open({
+    type: 1,
+    title: '修改文章分类',
+    content: html,
+    area: ['500px', '250px'],
+    success: function () {
+      let form = layui.form;
+      form.val("formTest", data);
+    }
+  });
+
+  $('form').on('submit', function (e) {
+    e.preventDefault();
+    let data = $(this).serialize();
+    $.ajax({
+      type: 'POST',
+      url: '/my/article/updatecate',
+      data: data,
+      success: function (res) {
+        layer.msg(res.message);
+        console.log(res.status);
+        if (res.status === 0) {
+          layer.close(index);
+          renderCategory();
+        }
+      }
+    });
+  })
+})
